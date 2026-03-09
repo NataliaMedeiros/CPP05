e7810c6793c41b3a49323b0466026a8d4aade055
Natália Medeiros Da Silva De Almeida <natalia@MacBookPro.home>
2026-03-09 11:38:31 +0100

    WIP

--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -20,26 +20,38 @@ class Bureaucrat
 
 		void		incrementGrade();
 		void		decrementGrade();
-};
 
-/* The subject says the exceptions must be catchable using try{} catch {}, and
-	for this to work, the class must inherit from std::exception.*/
-class GradeTooHighException : public std::exception
-{
-	private:
-		/* data */
-	public:
-		GradeTooHighException(/* args */);
-		~GradeTooHighException();
-};
+		/* The subject says the exceptions must be catchable using try{} catch {}, and
+			for this to work, the class must inherit from std::exception.
+			The exceptions are nested inside Bureaucrat, because these
+			exceptions belong to Bureaucrat and are used only for its errors.*/
+		class GradeTooHighException : public std::exception
+		{
+			private:
+				/* data */
+			public:
+				GradeTooHighException(/* args */);
+				~GradeTooHighException();
+			public:
+				const char* what() const throw()
+				{
+					return "Grade too high";
+				}
+		};
 
-class GradeTooLowException
-{
-	private:
-		/* data */
-	public:
-		GradeTooLowException(/* args */);
-		~GradeTooLowException();
+		class GradeTooLowException
+		{
+			private:
+				/* data */
+			public:
+				GradeTooLowException(/* args */);
+				~GradeTooLowException();
+			public:
+				const char* what() const throw()
+				{
+					return "Grade too low";
+				}
+		};
 };
 
 #endif

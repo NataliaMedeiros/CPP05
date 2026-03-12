4b986264d5ae6854978843abcbc5acf887c82ddf
Natália Medeiros Da Silva De Almeida <natalia@MacBookPro.home>
2026-03-12 19:46:10 +0100

    WIP

--- ex02/PresidentialPardonForm.cpp
+++ ex02/PresidentialPardonForm.cpp
@@ -2,8 +2,20 @@
 
 PresidentialPardonForm::PresidentialPardonForm(const std::string& target) : AForm("PresidentialPardonForm", 25, 5), target(target)
 {
-
+	std::cout << "*** PresidentialPardonForm was created***" << std::endl;
 }
-		PresidentialPardonForm(const PresidentialPardonForm& original);
-        PresidentialPardonForm& operator=(const PresidentialPardonForm& rhs);
-		~PresidentialPardonForm();
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm& original) : AForm(original), target(original.target)
+{
+	std::cout << "Cpy Constructor" << std::endl;
+}
+
+PresidentialPardonForm& PresidentialPardonForm::operator=(const PresidentialPardonForm& rhs)
+{
+	(void)rhs;
+	return (*this);
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+	std::cout << "***PresidentialPardonForm destroyed***" << std::endl;
+}
